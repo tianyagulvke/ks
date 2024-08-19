6584846915435881f9c0293c2c9474c3f709f532
MXL-ADMIN\ismxl <2234252173@qq.com>
2024-08-19 18:10:09 +0800

    feat: countlist

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,66 +1,70 @@
-import { createBrowserRouter, Navigate } from 'react-router-dom';
-import Layout from '@/pages/layout';
-import Home from '@/pages/home';
-import Mail from '@/pages/mail';
-import User from '@/pages/user';
-import Exam from '@/pages/exam';
-import ExamConfig from '@/pages/examconfig';
-import QuesConfig from '@/pages/quesconfig';
-import Err404 from '@/pages/error/404';
-import ImpQues from '@/pages/impques';
-import Check from '@/pages/exam/check';
-import Success from '@/pages/success';
-
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import Layout from "@/pages/layout";
+import Home from "@/pages/home";
+import Mail from "@/pages/mail";
+import User from "@/pages/user";
+import Exam from "@/pages/exam";
+import ExamConfig from "@/pages/examconfig";
+import QuesConfig from "@/pages/quesconfig";
+import Err404 from "@/pages/error/404";
+import ImpQues from "@/pages/impques";
+import Check from "@/pages/exam/check";
+import Success from "@/pages/success";
+import CountList from "@/pages/countlist";
 const routes = [
   {
-    path: '/',
+    path: "/",
     Component: Layout,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Navigate to="/home" replace />,
       },
       {
-        path: 'home',
+        path: "home",
         Component: Home,
       },
       {
-        path: 'mail',
+        path: "mail",
         Component: Mail,
       },
       {
-        path: 'user',
+        path: "user",
         Component: User,
       },
       {
-        path: 'exam',
+        path: "exam",
         Component: Exam,
       },
       {
-        path: 'examconfig',
+        path: "examconfig",
         Component: ExamConfig,
       },
       {
-        path: 'quesconfig',
+        path: "quesconfig",
         Component: QuesConfig,
       },
       {
-        path: '/impques',
+        path: "/impques",
         Component: ImpQues,
       },
       {
-        path: '/check',
+        path: "/check",
         Component: Check,
       },
       {
-        path: '/success',
+        path: "/success",
         Component: Success,
+      },
+      {
+        path: "/countlist",
+        Component: CountList,
       },
     ],
   },
 
   {
-    path: '*',
+    path: "*",
     Component: Err404,
   },
 ];
