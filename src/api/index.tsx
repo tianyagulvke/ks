41cb0901a27d5cb2b3d81a0dbd3aafdc70e175f5
MXL-ADMIN\ismxl <2234252173@qq.com>
2024-08-20 15:19:47 +0800

    feat: 8.20

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,1 +1,10 @@
-import { get, post } from '@/utils/request';
+import { get, post } from "@/utils/request";
+
+export const getAnswerRecordPage = (data?: object) =>
+  post("/answer/getAnswerRecordPage", data);
+
+export const readShortAnswerList = (data?: object) =>
+  post("/answer/readShortAnswerList", data);
+
+export const readAnswerDetail = (data?: object) =>
+  post("/answer/readAnswerDetail", data);
--- src/utils/request.tsx
+++ src/utils/request.tsx
@@ -1,51 +1,51 @@
-import axios from 'axios'
+import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: '/api',
-    timeout: 10000
-})
+  baseURL: "",
+  timeout: 10000,
+});
 
 // 请求拦截器
 
 axiosInstance.interceptors.request.use(
-    config => {
-        // const token = localStorage.getItem('token')
-        // if (token) {
-        //     config.headers.Authorization = `Bearer ${token}`
-        // } else {
-        //     throw new Error('Token not found')
-        // }
-        return config
-    },
-    error => {
-        return Promise.reject(error)
-    }
-)
+  (config) => {
+    // const token = localStorage.getItem('token')
+    // if (token) {
+    //     config.headers.Authorization = `Bearer ${token}`
+    // } else {
+    //     throw new Error('Token not found')
+    // }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 // 响应拦截器
 
 axiosInstance.interceptors.response.use(
-    response => {
-        return response
-    },
-    error => {
-        return Promise.reject(error)
-    }
-)
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 // 封装GET请求
 
 const get = (url: string, params = {}, options = {}) => {
-    return axiosInstance.get(url, {
-        params,
-        ...options
-    })
-}
+  return axiosInstance.get(url, {
+    params,
+    ...options,
+  });
+};
 
 // 封装post请求
 
 const post = (url: string, data = {}, options = {}) => {
-    return axiosInstance.post(url, data, options)
-}
+  return axiosInstance.post(url, data, options);
+};
 
-export {get, post, axiosInstance}
+export { get, post, axiosInstance };
