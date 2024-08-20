fd74070259a7eac42a9c419d59ef64694b9ad120
MXL-ADMIN\ismxl <2234252173@qq.com>
2024-08-20 09:23:49 +0800

    feat: 8.20

--- src/pages/countList/index.jsx
+++ src/pages/countList/index.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import { Table } from "antd";
 export default function CountList() {
+    const [dataSource, setDataSource] = useState([
+        {
+            key: '1',
+            name: '胡彦斌',
+            age: 32,
+            address: '西湖区湖底公园1号',
+        },
+        {
+            key: '2',
+            name: '胡彦祖',
+            age: 42,
+            address: '西湖区湖底公园1号',
+        },
+    ]);
+
+
+
+    const [columns, setColumns] = useState([
+        {
+            title: '姓名',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: '年龄',
+            dataIndex: 'age',
+            key: 'age',
+        },
+        {
+            title: '住址',
+            dataIndex: 'address',
+            key: 'address',
+        },
+    ]);
     return (
-        <div>CountList</div>
+        <div>
+            <Table dataSource={dataSource} columns={columns} />;
+        </div>
     )
 }
